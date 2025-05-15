120caf61e594b0f4beb4b85f090fdb4c88b5a4bd
Darren-SBA23255-CCT-L8 <sba23255@student.cct.ie>
2025-05-15 17:48:47 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-panel serve dashboard.ipynb --port=8888 --address=0.0.0.0 --allow-websocket-origin=localhost,127.0.0.1
+panel serve dashboard.ipynb --port=8888 --allow-websocket-origin=localhost,127.0.0.1
